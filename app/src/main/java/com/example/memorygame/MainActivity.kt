4584efe9b0227a269a5b2db917e88407f377f5fd
aalshawa97 <abdullahalshawa@gmail.com>
2021-11-01 17:41:37 -0700

    Game is functional

--- app/src/main/java/com/example/memorygame/MainActivity.kt
+++ app/src/main/java/com/example/memorygame/MainActivity.kt
@@ -4,32 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageButton
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import com.example.memorygame.R.drawable;
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+
     private lateinit var buttons: List<ImageButton>
     private lateinit var cards: List<MemoryCard>
+    private var indexOfSingleSelectedCard: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val images = mutableListOf(drawable.ic_blue_lightning, drawable.ic_code, drawable.ic_heart, drawable.ic_lightning, drawable.ic_plane, drawable.ic_smiley)
-        //Add each image twice so we can create pairs
+        val images = mutableListOf(R.drawable.ic_heart, R.drawable.ic_lightning, R.drawable.ic_plane, R.drawable.ic_smiley)
+        // Add each image twice so we can create pairs
         images.addAll(images)
-        //Randomize the order of images
+        // Randomize the order of images
         images.shuffle()
-        buttons = listOf(imageButton10, imageButton2, imageButton11, imageButton12, imageButton13, imageButton14, imageButton15, imageButton16)
-        cards = buttons.indices.map { index->
+
+        buttons = listOf(imageButton10, imageButton2, imageButton11, imageButton12, imageButton13,
+            imageButton14, imageButton15, imageButton16)
+
+        cards = buttons.indices.map { index ->
             MemoryCard(images[index])
         }
-        buttons.forEachIndexed{index, button ->
-            button.setOnClickListener{
-                Log.i(TAG, "button clicked!")
-                //Update models
+
+        buttons.forEachIndexed { index, button ->
+            button.setOnClickListener {
+                Log.i(TAG, "button clicked!!")
+                // Update models
                 updateModels(index)
-                //Update the UI for the game
+                // Update the UI for the game
                 updateViews()
             }
         }
@@ -38,12 +45,49 @@ class MainActivity : AppCompatActivity() {
     private fun updateViews() {
         cards.forEachIndexed { index, card ->
             val button = buttons[index]
-            button.setImageResource(if (card.isFaceUp) card.identifier else drawable.ic_code)
+            if (card.isMatched) {
+                button.alpha = 0.1f
+            }
+            button.setImageResource(if (card.isFaceUp) card.identifier else R.drawable.ic_code)
         }
     }
 
     private fun updateModels(position: Int) {
         val card = cards[position]
+        // Error checking:
+        if (card.isFaceUp) {
+            Toast.makeText(this, "Invalid move!", Toast.LENGTH_SHORT).show()
+            return
+        }
+        // Three cases
+        // 0 cards previously flipped over => restore cards + flip over the selected card
+        // 1 card previously flipped over => flip over the selected card + check if the images match
+        // 2 cards previously flipped over => restore cards + flip over the selected card
+        if (indexOfSingleSelectedCard == null) {
+            // 0 or 2 selected cards previously
+            restoreCards()
+            indexOfSingleSelectedCard = position
+        } else {
+            // exactly 1 card was selected previously
+            checkForMatch(indexOfSingleSelectedCard!!, position)
+            indexOfSingleSelectedCard = null
+        }
         card.isFaceUp = !card.isFaceUp
+    }
+
+    private fun restoreCards() {
+        for (card in cards) {
+            if (!card.isMatched) {
+                card.isFaceUp = false
+            }
+        }
+    }
+
+    private fun checkForMatch(position1: Int, position2: Int) {
+        if (cards[position1].identifier == cards[position2].identifier) {
+            Toast.makeText(this, "Match found!!", Toast.LENGTH_SHORT).show()
+            cards[position1].isMatched = true
+            cards[position2].isMatched = true
+        }
     }
 }
